2b7000fa92c64919f074beefd79a4099227800f1
leonamagaya <leonamagaya@gmail.com>
2023-11-03 12:57:39 +0100

    EJS Tags

---
+++ BackendDevelopment/4.0 EJS/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+
+
+var app = express();
+var port = 3000;
+
+
+
+app.get("/", (req, res) => {
+  const date = new Date();
+  const todaysDate = date.getDay();
+  const isWeekday = todaysDate >= 1 && todaysDate <= 5;
+  
+  res.render('index.ejs', { isWeekday });
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${3000}`);
+})
