2571ef0c0c37d9e31c7988e1bbf793cb60df2fe4
leonamagaya <leonamagaya@gmail.com>
2023-11-18 20:12:42 +0100

    fix: :fire: rerouted some files

--- BackendDevelopment/3.0 Node.js/2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-//import generateName from "sillyname";
-//var sillyName = generateName();
-
-//console.log(`My name is ${sillyName}`);
-
-import superheroes from "superheroes";
-var superHero = superheroes.random();
-
-console.log(`I am a super Hero called ${superHero}`);
